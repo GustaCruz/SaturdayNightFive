3c01ba349e0ac6917b250ddd0cfd4d5e90295735
GustaCruz <gustacruz10.gc@gmail.com>
2022-09-26 22:08:26 -0500

    Test for git push

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//This is a test For git compatibility 09/26/22
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
